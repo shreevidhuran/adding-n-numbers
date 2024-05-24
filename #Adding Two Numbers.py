1c3c74e292804aef88a3a329f95ceadc9dc81429
shreevidhuran <brillantmakers@gmail.com>
2024-05-24 12:32:41 +0530

    Add files via upload

---
+++ #Adding Two Numbers.py
@@ -0,0 +1,8 @@
+#Adding Two Numbers
+#Python programm to add two numbers
+Num1 = int(input("Enter the first number : "))
+Num2 = int(input("Enter the second number : "))
+#Adding two numbers
+sum=Num1+Num2
+#printing values
+print(f"sum of {Num1}and{Num2}is{sum}")
